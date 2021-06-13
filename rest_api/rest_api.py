243e6e49f4854e2405fcd8e0f9ba3fab5532db34
Fagonar96 <fagonar96.tec@gmail.com>
2021-06-13 02:12:38 -0600

    Maldito CORS

--- rest_api/rest_api.py
+++ rest_api/rest_api.py
@@ -1,26 +1,59 @@
-import json
+import json, base64
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
+cors = CORS(app, resources={r"/user": {"origins": "http://localhost:8100"}})
+cors = CORS(app, resources={r"/photo": {"origins": "http://localhost:8100"}})
+cors = CORS(app, resources={r"/settings": {"origins": "http://localhost:8100"}})
 
 photos = []
 
 @app.route('/user', methods=['PUT'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def put_user():
 
     user_metadata = request.get_json()
 
-    return '', 204
+    file = open("userjson.txt", "w")
+    file.write(json.dumps(user_metadata))
+    file.close()
+
+    response = ''
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response, 204
 
 @app.route('/user', methods=['POST'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def post_user():
 
     user_metadata = request.get_json()
+    request_email = user_metadata['email']
+    request_password = user_metadata['password']
+    with open("userjson.txt") as file:
+        user_string = file.read()
+        user_json = json.loads(user_string)
+        file.close()
+    file_name = user_json['name']
+    file_email = user_json['email']
+    file_password = user_json['password']
+    if ((request_email == file_email ) & 
+        (request_password == file_password)):
+        token = request_email + request_password
+        token_bytes = token.encode('ascii')
+        token_base64_bytes = base64.b64encode(token_bytes)
+        token_base64 = token_base64_bytes.decode('ascii')
+        response = {"name": file_name, "email": request_email, "password": request_password, "token": token_base64}
+        response = json.dumps(response)
+        print(response)
+        #response = json.loads(response)
 
-    return '', 204
+    return response, 204
 
 @app.route('/photo', methods=['POST'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def post_photo():
 
     photo_metadata = request.get_json()
@@ -28,28 +61,26 @@ def post_photo():
     return '', 204
 
 @app.route('/photo', methods=['GET'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def get_photos():
 
     return jsonify(photos)
 
-@app.route('/photo_delete', methods=['POST'])
-def delete_photo():
-
-    photo_metadata = request.get_json()
-    name = photo_metadata[0]['name']
+@app.route('/photo/<string:name>', methods=['DELETE'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
+def delete_photo(name):
     for photo in photos:
-        if photo[0]['name'] == name:
+        if photo['name'] == name:
             photos.remove(photo)
     return '', 204
 
 @app.route('/settings', methods=['POST'])
+@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def post_settings():
 
     settings = request.get_json()
-    max_face_distance = settings[0]['maxFaceDist']
-    known_max_face_distance = settings[0]['knownMaxFace']
-    print(max_face_distance)
-    print(known_max_face_distance)
+    print(settings['maxFaceDist'])
+    print(settings['knownMaxFace'])
     return '', 204
 
 if __name__ == "__main__":
